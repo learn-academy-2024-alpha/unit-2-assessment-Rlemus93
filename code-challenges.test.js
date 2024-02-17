2dbff53a56af76cf07440ccec5332f08a97fed02
Rlemus93 <rlemus93@gmail.com>
2024-02-16 17:02:08 -0800

    finished 2nd assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -10,22 +10,67 @@
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
 
+//Yarn template
+// describe("functionName", () => {
+//     it("what does the function do?", () => {
+//         expect(functionName()).toEqual(output)
+//     })
+// })
+
 // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multiplied by 3.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersArray1 = [6, 7, 8, 9, 10]
-// Expected output: [18, 21, 24, 27, 30]
-const numbersArray2 = [24, 27, 30, 33, 36]
-// Expected output: [72, 81, 90, 99, 108]
+// describe("multiply3", () => {
+//     it("multiplies each element in the array by 3 and returns in an array", () => {
+//         const numbersArray1 = [6, 7, 8, 9, 10]
+//         // Expected output: [18, 21, 24, 27, 30]
+//         const numbersArray2 = [24, 27, 30, 33, 36]
+//         // Expected output: [72, 81, 90, 99, 108]
+//         expect(multiply3(numbersArray1)).toEqual([18, 21, 24, 27, 30])
+//         expect(multiply3(numbersArray2)).toEqual([72, 81, 90, 99, 108])
+//     })
+// })
+//ReferenceError: multiply3 is not defined
 
 // b) Create the function that makes the test pass.
 
-// Explain your code:
+// input: array of numbers
+// output: array of numbers multiplied by 3
+// create a fxn that takes in an array and maps through each element.
+// returns each element multiplied by 3 
+
+const numbersArray1 = [6, 7, 8, 9, 10] 
+const numbersArray2 = [24, 27, 30, 33, 36] 
+
+const multiply3 = (array) => {
+    return array.map((value) => {
+        return value * 3
+    })
+}
+console.log(multiply3(numbersArray1)) //output: [ 18, 21, 24, 27, 30 ]
+console.log(multiply3(numbersArray2)) //output: [ 72, 81, 90, 99, 108 ]
+
+// Explain your code: I created a function that maps through each element of the array and takes each element and multiplies it by 3. giving the new array with the output on lines 44 and 45. 
 
 // --------------------1) Create a function that takes a object as an argument and decides if the number inside it is evenly divisible by three or not.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+// describe("divisibleby3", () => {
+//     it("take the object as argument and decides if the number is evenly divivisble by 3", () => {
+//         const object1 = { number: 15 }
+//         // Expected output: "15 is divisible by three"
+//         const object2 = { number: 0 }
+//         // Expected output: "0 is divisible by three"
+//         const object3 = { number: -7 }
+//         // Expected output: "-7 is not divisible by three"
+//         expect(divisibleby3(object1)).toEqual("15 is divisible by three")
+//         expect(divisibleby3(object2)).toEqual("0 is divisible by three")
+//         expect(divisibleby3(object3)).toEqual("-7 is not divisible by three")
+//     })
+// })
+//ReferenceError: divisibleby3 is not defined
 
 const object1 = { number: 15 }
 // Expected output: "15 is divisible by three"
@@ -34,19 +79,61 @@ const object2 = { number: 0 }
 const object3 = { number: -7 }
 // Expected output: "-7 is not divisible by three"
 
-// b) Create the function that makes the test pass.
+// // b) Create the function that makes the test pass.
 
-// Explain your code:
+// input: object with a key that is a number
+// output: a string that tells us if the number is evenly divisible by three
+// create a fxn that takes the object as a parameter and iterates through the value given as the argument
+// return whether that number is evenly divisible by 3. using modulo operator 
+
+const divisibleby3 = (object) => {
+    if (object.number % 3 === 0) { 
+        return `${object.number} is divisible by three` 
+    } else {
+        return `${object.number} is not divisible by three`
+    }
+}
+console.log(divisibleby3(object1)) //output: 15 is divisible by three
+console.log(divisibleby3(object2)) //output: 0 is divisible by three
+console.log(divisibleby3(object3)) //output: -7 is not divisible by three
+//Expected: "15 is divisible by three"   Received: "undefined is divisible by three". kept getting this within my test. and was wondering why. figured it out because i was putting this.number instead of object.number. then remembered i had the parameter object! 
+
+// Explain your code: the code above uses a condition that will take the key:number and the value: 15, 0 , 7 and takes it through the equation to tell us if that number is divisible by three or not. 
 
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
-// Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
-const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
-// Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+// describe("capArray", () => {
+//     it("takes in array of strings and returns array of strings with all capitalized words", () => {
+//         const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+//         // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+//         const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+//         // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+//         expect(capArray(randomNouns1)).toEqual(["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"])
+//         expect(capArray(randomNouns2)).toEqual(["Temperature", "Database", "Chopsticks", "Mango"])
+//     })
+// })
+// ReferenceError: capArray is not defined
 
 // b) Create the function that makes the test pass.
 
-// Explain your code:
+// input: Array of strings
+// output: Array of strings with each word capitalized
+// create a fxn that maps through each element of the array. takes the 0 index of that element and uppercases that letter. 
+// combine that index with the rest of the word to the capitilized letter
+// return the new array.
+
+const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
+        // Expected output: ["Streetlamp", "Potato", "Teeth", "Conclusion", "Nephew"]
+        const randomNouns2 = ["temperature", "database", "chopsticks", "mango"]
+        // Expected output: ["Temperature", "Database", "Chopsticks", "Mango"]
+
+const capArray = (array) => {
+    return array.map(letter => letter[0].toUpperCase() + letter.slice(1))
+}
+
+console.log(capArray(randomNouns1)); //output: [ 'Streetlamp', 'Potato', 'Teeth', 'Conclusion', 'Nephew' ]
+console.log(capArray(randomNouns2)); //output: [ 'Temperature', 'Database', 'Chopsticks', 'Mango' ] 
+
+// Explain your code: used a hof on this one! I took my parameter array and used .map to map through the array. then i set the parameter letter to take the first letter of each word [0] and uppercase the first letter of each word. then i used .slice with index [1] to add the rest of the letters back to the uppercase letters. used (1) so it started at the second character. at first i was using (0) and in my test it was giving me. [ 'Sstreetlamp', 'Ppotato', 'Tteeth', 'Cconclusion', 'Nnephew' ] [ 'Ttemperature', 'Ddatabase', 'Cchopsticks', 'Mmango' ]. 
--- coding-concepts.js
+++ coding-concepts.js
@@ -9,28 +9,29 @@
 // --------------------1) What will this log?
 
 const cohort = "Alpha 2024"
-// console.log(cohort.split(" "))
+//console.log(cohort.split(" "))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "Alpha" "2024"
+// b) Verify and explain: [ 'Alpha', '2024' ]. so im right but wrong! had to remember that .split returns a new array of the string. it also puts the string into a substrings but within side a new array. 
 
 // --------------------2) What will this log?
 
 const greeter = (name) => {
   `Hello, ${name}!`
 }
-// console.log(greeter("LEARN Student"))
+//console.log(greeter("LEARN Student"))
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: undefined. because name is the parameter not the variable. and there is no value to a variable with name. so using string interpolation wont work.
+// b) Verify and explain: got this one right. these is just no value here with a name when you are console logging. you would need a variable called name with a value for this to work. 
 
 // --------------------3) What will this log?
 
 const onlyOdds = [11, 12, 13, 14, 15].filter((number) => number % 2 !== 0)
-// console.log(onlyOdds)
+//console.log(onlyOdds)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: all the odd numbers. [11, 13, 15]
+// b) Verify and explain: awesome! so my understanding of this is since its using hof .filter it is then taking each number(element) through the equation number % 2 !== 0 and leaves us with the odd numbers. if it was ===0 it would give us the even numbers
+
 
 // --------------------4) What will this log?
 
@@ -40,22 +41,22 @@ const myCodingSkills = {
   databases: "PostgreSQL",
   versionControl: "GitHub"
 }
-// console.log(myCodingSkills.languages[0])
+ //console.log(myCodingSkills.languages[0])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: JavaScript
+// b) Verify and explain: So this one is telling us to look at the key which is languages by using dot notation and then specifically what is at [0] index of that key. which gives the value JavaScript since its at the [0] index of the key languages
 
 // --------------------5) What will this log?
 
 class Learn {
-  constructor(name) {
+  constructor(name, cohort, year) {
     this.student = name
-    this.cohort = "Alpha"
-    this.year = 2024
+    this.cohort = cohort //"Alpha"
+    this.year = year //2024
   }
 }
-const learnStudent = new Learn("George")
-// console.log(learnStudent)
+const learnStudent = new Learn("George","Bravo", 2040)
+console.log(learnStudent)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: {student: George cohort: Alpha year: 2024}
+// b) Verify and explain: so it printed Learn { student: 'George', cohort: 'Alpha', year: 2024 }. i was not fully right becuase it also printed out Learn. from what i remember on classes is when you are setting a variable learnStudent = new Learn this is giving you the option to change what you have in the class but it depends on what you have the parameters set to in the constructor. so since name is the only parameter thats why its only changing this.student. i beleive if you add cohort and year as parameters in the constructor and then set the value in the keys to cohort and year. you would be able to add onto line 59 to change it all. im going to try it. Output for that is //Learn { student: 'George', cohort: 'Bravo', year: 2040 }
